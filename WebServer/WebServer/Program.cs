253fc9463779fd2db5139ce6a7225f100d09b98a
YourRoyalLinus <jonbillings1996@gmail.com>
2020-05-20 20:57:48 -0400

    initial commit

---
+++ WebServer/WebServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
